88d0e6eff9309e390be0038aa46d4d93fdac9a1a
Yash Sharma <95yashsharma@gmail.com>
2016-08-31 20:55:08 +0530

    sample

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+
+    // load streetview
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+    // load nytimes
+    
+    // YOUR CODE GOES HERE!
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
